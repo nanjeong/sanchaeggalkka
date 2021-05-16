f0e83bd49620d0b921f6668710742f8838814445
nanjeong <whdwkxj7@naver.com>
2021-05-16 23:30:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sanchaeggalkka/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.sanchaeggalkka
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sanchaeggalkka.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.small.setOnClickListener {
+            val smallIntent = Intent(this, WeatherActivity::class.java)
+
+            smallIntent.putExtra("size", "small")
+
+            startActivity(smallIntent)
+        }
+
+        binding.medium.setOnClickListener {
+            val mediumIntent = Intent(this, WeatherActivity::class.java)
+
+            mediumIntent.putExtra("size", "medium")
+
+            startActivity(mediumIntent)
+        }
+
+        binding.large.setOnClickListener {
+            val largeIntent = Intent(this, WeatherActivity::class.java)
+
+            largeIntent.putExtra("size", "large")
+
+            startActivity(largeIntent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sanchaeggalkka/WeatherActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sanchaeggalkka
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.example.sanchaeggalkka.databinding.ActivityWeatherBinding
+
+class WeatherActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWeatherBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWeatherBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val size = intent.getStringExtra("size")
+
+        when (size) {
+            "small" -> {
+                binding.explain.background = ResourcesCompat.getDrawable(resources, R.color.small_size, null)
+            }
+            "medium" -> {
+                binding.explain.background = ResourcesCompat.getDrawable(resources, R.color.medium_size, null)
+            }
+            "large" -> {
+                binding.explain.background = ResourcesCompat.getDrawable(resources, R.color.large_size, null)
+            }
+        }
+    }
+}
